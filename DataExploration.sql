89ba72dfbf2255d0361106e7eabda4e6420de302
sushant-97 <65329646+sushant-97@users.noreply.github.com>
2022-06-15 07:31:31 +0530

    Update DataExploration.sql

--- DataExploration.sql
+++ DataExploration.sql
@@ -1,5 +1,6 @@
 /*
 Covid 19 Data Exploration 
+Data is downloaded from https://ourworldindata.org/covid-deaths
 Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
 */
 
